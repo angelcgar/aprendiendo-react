fcbdb36f912e4e0afb8f712691ff6b7d4ae14aea
angelcgar <146035436+angelcgar@users.noreply.github.com>
2023-12-28 15:02:52 -0600

    probar de nuevo eslint

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,42 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [enable, setEnable] = useState(false);
+	const [enable, setEnable] = useState(false);
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+	console.log(position);
 
-  useEffect(() => {
-    console.log("Efect", { enable });
+	useEffect(() => {
+		console.log('Efect', { enable });
 
-    const hanleMove = (e) => {
-      const { clientx, clienty } = e;
-      console.log("cursor", clientx, clienty);
-    };
+		const hanleMove = (event) => {
+			const { clientx, clienty } = event;
+			console.log('cursor', clientx, clienty);
+			setPosition({ x: clientx, y: clienty });
+		};
 
-    if (enable) {
-      window.addEventListener("pointermove", hanleMove);
-    }
-  }, [enable]);
+		if (enable) {
+			window.addEventListener('pointermove', hanleMove);
+		}
 
-  return (
-    <main>
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "#09f",
-          borderRadius: "50%",
-          opacity: 0.8,
-          pointerEvents: "none",
-          left: -25,
-          top: -25,
-          width: 50,
-          height: 50,
-          transition: "translate(0px, 0px)",
-        }}
-      />
-      <button onClick={() => setEnable(!enable)}>
-        {enable ? "desactivar" : "activar"} seguir puntero
-      </button>
-    </main>
-  );
+		return () => {
+			console.log('cleanup');
+			window.removeEventListener('pointermove', hanleMove);
+		};
+	}, [enable]);
+
+	return (
+		<main>
+			<div
+				style={{
+					position: 'absolute',
+					backgroundColor: '#09f',
+					borderRadius: '50%',
+					opacity: 0.8,
+					pointerEvents: 'none',
+					left: -25,
+					top: -25,
+					width: 50,
+					height: 50,
+					transition: `translate(${position.x}px, ${position.y}px)`,
+				}}
+			/>
+			<button onClick={() => setEnable(!enable)}>
+				{enable ? 'desactivar' : 'activar'} seguir puntero
+			</button>
+		</main>
+	);
 }
 
 export default App;
