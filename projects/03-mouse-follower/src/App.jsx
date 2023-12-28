f6a4c7d4bf741298dd41eaaffa23441ff039e36d
angelcgar <146035436+angelcgar@users.noreply.github.com>
2023-12-28 14:02:25 -0600

    empezar tercer proyecto, no se lo que estoy haciendo

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,7 +1,42 @@
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <h1>Proyecto 3</h1>;
+  const [enable, setEnable] = useState(false);
+
+  useEffect(() => {
+    console.log("Efect", { enable });
+
+    const hanleMove = (e) => {
+      const { clientx, clienty } = e;
+      console.log("cursor", clientx, clienty);
+    };
+
+    if (enable) {
+      window.addEventListener("pointermove", hanleMove);
+    }
+  }, [enable]);
+
+  return (
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transition: "translate(0px, 0px)",
+        }}
+      />
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? "desactivar" : "activar"} seguir puntero
+      </button>
+    </main>
+  );
 }
 
 export default App;
